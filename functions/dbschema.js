dbc6eb8003035ef285902e19b86452b15a1bd1ae
KeithC113 <campbell_ka@yahoo.co.uk>
2020-11-16 13:50:35 +0000

    dbschema added. Date stamp changed

---
+++ functions/dbschema.js
@@ -0,0 +1,14 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-11-16T13:45:55.996Z',
+            likeCount: 5, 
+            commentCount: 2
+
+        }
+
+    ]
+
+}
