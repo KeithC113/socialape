a040f45ed969873500f31c8d68dc8fbf22cbf697
KeithC113 <campbell_ka@yahoo.co.uk>
2020-11-16 14:43:56 +0000

    user login added

--- functions/index.js
+++ functions/index.js
@@ -19,9 +19,10 @@ const firebaseConfig = {
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig); 
 
+const db = admin.firestore(); 
 
 app.get('/screams', (req, res) => {
-    admin.firestore()
+    db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -48,7 +49,7 @@ app.post('/scream', (req, res) =>{
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -73,14 +74,26 @@ app.post('/signup', (req, res) => {
 
 // Validate data 
 
-firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-    .then(data => {
-        return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`}); 
-    })
-    .catch((err) => {
-        console.error(err); 
-        return res.status(500).json({ error: err.code}); 
-    })
+db.doc(`/users/${newUser.handle}`).get()
+.then(doc => {
+    if(doc.exists){
+        return res.status(400).json({ handle: 'this handle is already taken'});
+    } else{
+      return firebase
+       .auth()
+       .createUserWithEmailAndPassword(newUser.email, newUser.password) 
+    }
 })
+.then(data => { 
+    return data.user.getIdToken()
+})
+.then(token => {
+    return res.status(201).json({token}); 
+})
+.catch(err => {
+    console.error(err); 
+    return res.status(500).json({error: err.code}); 
+    });
+});
 
 exports.api = functions.https.onRequest(app); 
